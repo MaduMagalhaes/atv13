819589c76b4e744756793e31f99c118b46f41991
madumagalhaes <139500396+MaduMagalhaes@users.noreply.github.com>
2024-03-26 08:34:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,88 @@
 const express = require('express');
 const app = express();
-
-app.set('view engine', 'ejs');
+const bodyParser = require('body-parser');
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render('index', { livros: livros });
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/login', (req, res) => {
+    const errorMessage = req.query.error ? 'Credenciais inválidas! Tente novamente.' : null;
+    res.render('login', { error: errorMessage });
 });
 
-let livros = [
-  { id: 1, titulo: 'Título 1', nome: 'Fulano 1', ano: 2000},
-  { id: 2, titulo: 'Título 2', nome: 'Fulano 2', ano: 2000},
-  { id: 3, titulo: 'Título 3', nome: 'Fulano 3', ano: 2000},
-  { id: 4, titulo: 'Título 4', nome: 'Fulano 4', ano: 2000},
-  { id: 5, titulo: 'Título 5', nome: 'Fulano 5', ano: 2000},
-  { id: 6, titulo: 'Título 6', nome: 'Fulano 6', ano: 2000},
-  { id: 7, titulo: 'Título 7', nome: 'Fulano 7', ano: 2001}
-  
+const users = [
+    { id: 1, email: 'usuario1@example.com', password: 'senha1' },
+    { id: 2, email: 'usuario2@example.com', password: 'senha2' },
+    { id: 3, email: 'usuario3@example.com', password: 'senha3' }
 ];
 
-app.get('/buscar/:ano', (req, res) => {
-  let livroAno = req.params.ano;
-  let filtragemId = livros.filter(livro => livro.ano == livroAno);
-  res.render('index', {livros: filtragemId});
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    const user = users.find(user => user.email === email && user.password === password);
+    if (user) {
+        res.redirect('/login/success');
+    } else {
+        res.redirect('/login?error=true');
+    }
 });
 
-app.get('/buscar', (req, res) => {
-  let titulo = req.query.titulo;
-  let filtragemTitulo = livros.filter(livro => livro.titulo.toLowerCase() === titulo.toLowerCase());
-  res.render('index', { livros: filtragemTitulo });
-  
+app.get('/login/success', (req, res) => {
+    res.render('login-success');
 });
 
+app.get('/register', (req, res) => {
+    const errorMessage = req.query.error ? 'Erro ao registrar usuário. Tente novamente.' : null;
+    res.render('register', { error: errorMessage });
+});
+
+app.post('/register', (req, res) => {
+    const { name, email, password, confirmPassword } = req.body;
+    if (!name || !email || !password || !confirmPassword) {
+        return res.redirect('/register?error=Todos os campos são obrigatórios.');
+    }
+    const emailRegex = /\S+@\S+\.\S+/;
+    if (!emailRegex.test(email)) {
+        return res.redirect('/register?error=E-mail inválido. Tente novamente.');
+    }
+    if (password !== confirmPassword) {
+        return res.redirect('/register?error=As senhas não coincidem. Tente novamente.');
+    }
+    if (users.some(user => user.users === users)) {
+        res.render('register', { error: 'Usuário já cadastrado' }); // Definindo a variável error
+    } else {
+        const newUser = {
+            id: users.length + 1,
+            name,
+            email,
+            password,
+            confirmPassword
+        };
+        users.push(newUser);
+    }
+    res.redirect('/login');
+});
+
+app.use(cookieParser());
+app.use(session({
+    secret:'yourSecret',
+    resave: false,
+    saveUnitialized: false,
+    cookie: {
+        maxAge: 1000*60*60*24,
+        httpOnly: true
+    }
+}));
+
+const logout = (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/login');
+    });
+};
+
+// Iniciar o servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
